c6091290e5c3812c1ad46757bb7a0362ec08c0c8
tahirs95 <tahirs95@hotmail.com>
2020-05-14 00:43:05 +0500

    fix

--- app.py
+++ app.py
@@ -4,20 +4,23 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 urls = []
+in_process = False
 
 def process(links):
     global urls
+    global in_process
+    in_process = True
     urls = []
     for link in links: 
         r = requests.get(link)
         if r.status_code == 404:
             urls.append(link)
+    in_process = False
     
 
 @app.route("/", methods=['GET'])
 def process_all():
-    print("ss", threading.active_count())
-    if threading.active_count() > 3:
+    if in_process:
         return jsonify({"message": "Threads are busy."})
     links = []
     with open('links.txt') as f:
@@ -29,7 +32,7 @@ def process_all():
 
 @app.route("/", methods=['POST'])
 def process_some():
-    if threading.active_count() > 3:
+    if in_process:
         return jsonify({"message": "Threads are busy."})
     if request.method == "POST":
         body = json.loads(request.data)
